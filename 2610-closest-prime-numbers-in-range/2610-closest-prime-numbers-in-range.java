376eb10cb58b7ba701ed65762d9ed37129872187
Hariom Sharma <122799261+Hariom-sharma01@users.noreply.github.com>
2025-03-07 14:58:40 +0530

    Time: 102 ms (48.8%), Space: 50.4 MB (32%) - LeetHub

--- 2610-closest-prime-numbers-in-range/2610-closest-prime-numbers-in-range.java
+++ 2610-closest-prime-numbers-in-range/2610-closest-prime-numbers-in-range.java
@@ -1,9 +1,18 @@
 class Solution {
     public int[] closestPrimes(int left, int right) {
-        int[] ans=new int[2];
+        boolean[] isPrime=new boolean[right+1];
+        Arrays.fill(isPrime,true);
+        isPrime[0]=isPrime[1]=false;
+        for(int i=2;i*i<=right;i++){
+            if(isPrime[i]){
+                for(int j=i*i;j<=right;j+=i){
+                    isPrime[j]=false;
+                }
+            }
+        }
         ArrayList<Integer> ls=new ArrayList<>();
         for(int i=left;i<=right;i++){
-            if(isPrime(i))ls.add(i);
+            if(isPrime[i])ls.add(i);
         }
         if(ls.size()<2)return new int[] { -1, -1 };;
         int d1=ls.get(0);
@@ -17,12 +26,5 @@ class Solution {
         }
 
         return new int[] {d1,d2};
-    }
-    public static boolean isPrime(int n){
-        if(n==1)return false;
-        for(int i=2;i<=Math.sqrt(n);i++){
-            if(n%i==0)return false;
-        }
-        return true;
     }
 }
