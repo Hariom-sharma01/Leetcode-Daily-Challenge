4f31df7c0d2c1d0cc4acf652ecd6541815c8fa13
Hariom Sharma <122799261+Hariom-sharma01@users.noreply.github.com>
2025-04-14 21:34:28 +0530

    Time: 764 ms (51%), Space: 55.7 MB (26.39%) - LeetHub

--- 2150-kth-smallest-product-of-two-sorted-arrays/2150-kth-smallest-product-of-two-sorted-arrays.java
+++ 2150-kth-smallest-product-of-two-sorted-arrays/2150-kth-smallest-product-of-two-sorted-arrays.java
@@ -1,59 +1,60 @@
 class Solution {
     public long kthSmallestProduct(int[] nums1, int[] nums2, long k) {
-        return KthSmallestPossible(nums1, nums2, k);
-    }
+        long low = -1000_000_0001l;
+        long high = 1000_000_0001l;
+        long ans =0;
 
-    public long KthSmallestPossible(int[] nums1, int[] nums2, long k) {
-        long lo = -1_000_000_000_000_000_000L; // Lower bound for product
-        long hi = 1_000_000_000_000_000_000L;  // Upper bound for product
-        long ans = 0;
+        while(low <= high){
+            long mid = (low+high)/2;
 
-        while (lo <= hi) {
-            long mid = lo + (hi - lo) / 2;
-            if (countProduct(nums1, nums2, mid) >= k) {
+            if(countProduct(nums1, nums2, mid)>=k){
                 ans = mid;
-                hi = mid - 1;
-            } else {
-                lo = mid + 1;
+                high = mid-1;
             }
+            else
+                low = mid+1;
         }
 
         return ans;
     }
 
-    public long countProduct(int[] nums1, int[] nums2, long target) {
-        long count = 0;
+    public static long countProduct(int[]nums1, int[]nums2, long dot_pro){
+        long ans =0;
 
-        for (int a : nums1) {
-            if (a == 0) {
-                if (target >= 0) count += nums2.length;
-            } else if (a > 0) {
-                int low = 0, high = nums2.length - 1, best = -1;
-                while (low <= high) {
-                    int mid = low + (high - low) / 2;
-                    if ((long) a * nums2[mid] <= target) {
-                        best = mid;
-                        low = mid + 1;
-                    } else {
-                        high = mid - 1;
+        for(int e1: nums1){
+            int c =0;
+            int low =0;
+            int high = nums2.length-1;
+            if(e1>=0){
+                while(low<= high){
+                    int mid = (low+high)/2;
+                    if((long)e1*nums2[mid] <= dot_pro){
+                        c= mid+1;
+                        low = mid+1;
+                    }
+                    else{
+                        high = mid-1;
                     }
                 }
-                count += best + 1;
-            } else {
-                int low = 0, high = nums2.length - 1, best = nums2.length;
-                while (low <= high) {
-                    int mid = low + (high - low) / 2;
-                    if ((long) a * nums2[mid] <= target) {
-                        best = mid;
-                        high = mid - 1;
-                    } else {
-                        low = mid + 1;
+
+                ans += c;
+            }
+
+            else{
+                while(low<= high){
+                    int mid = (low+high)/2;
+                    if((long)e1*nums2[mid] <= dot_pro){
+                        c=nums2.length-mid;
+                        high = mid-1;
+                    }
+                    else{
+                        low = mid+1;
                     }
                 }
-                count += nums2.length - best;
+
+                ans += c;
             }
         }
-
-        return count;
+        return ans;
     }
 }
