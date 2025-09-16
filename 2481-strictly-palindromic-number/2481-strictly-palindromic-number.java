d4ac21f49ef47915fed5bfc9520f8d81a55d5c24
Hariom Sharma <122799261+Hariom-sharma01@users.noreply.github.com>
2025-09-16 22:22:00 +0530

    Time: 0 ms (100%), Space: 40.5 MB (41.87%) - LeetHub

--- 2481-strictly-palindromic-number/2481-strictly-palindromic-number.java
+++ 2481-strictly-palindromic-number/2481-strictly-palindromic-number.java
@@ -1,5 +1,26 @@
 class Solution {
     public boolean isStrictlyPalindromic(int n) {
+        for(int i=2;i<=n-2;i++){
+        if(!palindrome(baseConverter(n,i))){
+            return false;
+        }
+       }
+       return true;
+    }
+    public String baseConverter(int n,int base){
+        StringBuilder result=new StringBuilder("");
+        while(n>0){
+            int rem=n%base;
+            result.insert(0,rem);
+            n=n/2;
+        }
+        return result.toString();
+    }
+    public boolean palindrome(String s){
+        StringBuilder sb=new StringBuilder(s);
+        if(s==sb.reverse().toString()){
+            return true;
+        }
         return false;
     }
 }
